8d6752e59afa964492e33e0dd689d68e882c2c44
zestairlove <violetdessin@gmail.com>
2019-02-07 00:56:19 +0900

    add counterApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import CounterApp from './Apps/CounterApp';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Learn React
+        <CounterApp />
       </div>
     );
   }
---
+++ src/Apps/CounterApp/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Counter from './components/Counter';
+
+class CounterApp extends Component {
+  state = {
+    number: 0
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  handleDecrement = () => {
+    this.setState({
+      number: this.state.number - 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Counter
+          number={this.state.number}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
+      </div>
+    )
+  }
+};
+
+export default CounterApp;
